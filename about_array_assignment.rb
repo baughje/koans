a34812930a7b20c9d9566d7912d0c19cf6d7ae6f
Jeb <jeb.baugh+git@gmail.com>
2012-02-18 19:45:15 -0600

    about_array_assigment

--- about_array_assignment.rb
+++ about_array_assignment.rb
@@ -3,13 +3,13 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 class AboutArrayAssignment < EdgeCase::Koan
   def test_non_parallel_assignment
     names = ["John", "Smith"]
-    assert_equal __, names
+    assert_equal ["John", "Smith"], names
   end
 
   def test_parallel_assignments
     first_name, last_name = ["John", "Smith"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal "Smith", last_name
   end
 
   def test_parallel_assignments_with_extra_values
