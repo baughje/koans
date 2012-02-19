bd45830096498e37a10a79ddd754a9086d22cda1
Jeb <jeb.baugh+git@gmail.com>
2012-02-18 21:14:00 -0600

    finished about_array_assignment.rb

--- about_array_assignment.rb
+++ about_array_assignment.rb
@@ -32,20 +32,20 @@ class AboutArrayAssignment < EdgeCase::Koan
 
   def test_parallel_assignments_with_subarrays
     first_name, last_name = [["Willie", "Rae"], "Johnson"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal ["Willie", "Rae"], first_name
+    assert_equal "Johnson", last_name
   end
 
   def test_parallel_assignment_with_one_variable
     first_name, = ["John", "Smith"]
-    assert_equal __, first_name
+    assert_equal "John", first_name
   end
 
   def test_swapping_with_parallel_assignment
     first_name = "Roy"
     last_name = "Rob"
     first_name, last_name = last_name, first_name
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "Rob", first_name
+    assert_equal "Roy", last_name
   end
 end
